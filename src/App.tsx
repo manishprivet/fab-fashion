04748b4c0e6033f7b90a7fcb61ad69d6c6069feb
Manish Kumar <manishprivet@protonmail.com>
2020-09-18 11:54:16 +0530

    Added Auth and Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import Homepage from './pages/Homepage/Homepage';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Shop from './pages/Shop/Shop';
 import Header from './components/Header/Header';
 import SignInAndUp from './pages/SignInAndUp/SignInAndUp';
 import { auth, createUserProfileDocument } from './utils/firebase';
 import { setCurrentUser } from './redux/user/userActions';
 import { useDispatch } from 'react-redux';
+import { useTypedSelector } from './redux/rootReducer';
 
 function App() {
   const dispatch = useDispatch();
+  const currentUser = useTypedSelector((state) => state.user.currentUser);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -32,7 +34,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shop} />
-        <Route path="/signin" component={SignInAndUp} />
+        <Route
+          path="/signin"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignInAndUp />)}
+        />
       </Switch>
     </div>
   );
